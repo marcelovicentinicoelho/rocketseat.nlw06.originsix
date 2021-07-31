b1385f069b2ba261f00a62cbddd77ef0e19f5131
Marcelo Coelho <marcelo.coelho@softvision.com.br>
2021-07-30 23:36:42 -0300

    c5 - Aula 05 | Surface Exploration

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 /* Abre e fecha o menu quando clicar nos ícones "hamburger" e "x" */
 const nav = document.querySelector("#header nav");
 const toggle = document.querySelectorAll("nav .toggle");
-
 for (const element of toggle) {
   element.addEventListener("click", function () {
     nav.classList.toggle("show");
@@ -10,7 +9,6 @@ for (const element of toggle) {
 
 /* Esconder o menu quando um dos itens for clicado */
 const links = document.querySelectorAll("nav ul li a");
-
 for (const link of links) {
   link.addEventListener("click", function () {
     nav.classList.remove("show");
@@ -18,10 +16,9 @@ for (const link of links) {
 }
 
 /* colocar sombra no header da página quando der scroll */
+const header = document.querySelector("#header");
+const navheight = header.offsetHeight; // Deslocamento da altura
 function addShadowHeaderWhenScroll() {
-  const header = document.querySelector("#header");
-  const navheight = header.offsetHeight; // Deslocamento da altura
-
   if (window.scrollY >= navheight) {
     header.classList.add("scroll");
   } else {
@@ -37,6 +34,12 @@ const swiper = new Swiper(".swiper-container", {
   },
   mousewheel: true,
   keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true,
+    },
+  },
 });
 
 /* Scroll Reveal: mostrar elementos suavemente ao usar a barra de rolagem */
@@ -60,8 +63,8 @@ scrollReveal.reveal(
 );
 
 /* Botão Back to Top */
+const backToTopButton = document.querySelector(".back-to-top");
 function backToTop() {
-  const backToTopButton = document.querySelector(".back-to-top");
   window.addEventListener("scroll", function () {
     if (window.scrollY >= 560) {
       backToTopButton.classList.add("show");
@@ -71,8 +74,34 @@ function backToTop() {
   });
 }
 
+/* Menu ativo conforme a seção visível na página */
+const sections = document.querySelectorAll("main section[id]");
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute("id");
+
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  }
+}
+
 /* When scroll */
 window.addEventListener("scroll", function () {
   addShadowHeaderWhenScroll();
   backToTop();
+  activateMenuAtCurrentSection();
 });
